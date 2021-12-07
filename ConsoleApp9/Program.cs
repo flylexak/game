e9191d772778a3c9bdc67fef6d9ed605110821ff
Semen06 <94125314+Semen06@users.noreply.github.com>
2021-12-07 19:13:24 +0400

    code
    Co-Authored-By: flylexak <94125401+flylexak@users.noreply.github.com>

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, size1;
+            Console.WriteLine("Введите размерность массива");
+            string inputedSize = Console.ReadLine();
+            size = int.Parse(inputedSize);
+            size1 = int.Parse(inputedSize);
+
+            string[,] names = new string[size, size1];
+            string[,] familes = new string[size, size1];
+
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Введите имя №{i + 1}");
+                names[i, 0] = Console.ReadLine();
+
+            }
+
+            for (int n = 0; n < size1; n++)
+            {
+                Console.WriteLine($"Введите фамилию №{n + 1}");
+                familes[n, 0] = Console.ReadLine();
+
+            }
+            Console.Clear();
+            Console.WriteLine("Все фамилии и имена");
+
+
+
+
+            foreach (var nm in names)
+            {
+
+                for (int l = 0; l < 1; l++)
+                {
+                    Console.WriteLine(nm + $"-имя №{l + 1}");
+                }
+            }
+
+            foreach (string nm in familes)
+            {
+
+                for (int n = 0; n < 1; n++)
+                {
+                    Console.WriteLine(nm + $"-фамилия №{n + 1}");
+                }
+            }
+
+
+            foreach (string nm in names)
+            {
+
+                Console.WriteLine(":)");
+            }
+
+
+        }
+    }
+}
