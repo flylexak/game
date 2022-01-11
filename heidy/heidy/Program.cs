d509aa490a2cba55efdb197904fb9eb466ba437b
flylexak <94125401+flylexak@users.noreply.github.com>
2022-01-11 20:03:35 +0400

    new sql

---
+++ heidy/heidy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Npgsql;
+
+namespace heidy
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var connection = new NpgsqlConnection();
+      connection.ConnectionString = "Host=10.46.1.131; Port=5432; Database=ITB15; Username=postgres; Password=12345";
+      connection.Open();
+      var command = connection.CreateCommand();
+      command.CommandText = "INSERT INTO \"qwzc.events\" (\"ID\", \"Events\") VALUES (17, 'HappyBirthday'), (27, 'LoveDay'), (37, 'NewYear')";
+      command.ExecuteNonQuery();
+    }
+  }
+}
