f65b0310670433959994254a5567c431dd6f81d6
flylexak <94125401+flylexak@users.noreply.github.com>
2022-01-13 19:34:48 +0400

    Update Program.cs

--- heidy/heidy/Program.cs
+++ heidy/heidy/Program.cs
@@ -11,8 +11,16 @@ namespace heidy
       connection.ConnectionString = "Host=10.46.1.131; Port=5432; Database=ITB15; Username=postgres; Password=12345";
       connection.Open();
       var command = connection.CreateCommand();
-      command.CommandText = "INSERT INTO \"qwzc.events\" (\"ID\", \"Events\") VALUES (17, 'HappyBirthday'), (27, 'LoveDay'), (37, 'NewYear')";
-      command.ExecuteNonQuery();
+      command.CommandText = @"SELECT * FROM ""qwzc.dates""";
+      var reader = command.ExecuteReader();
+      while (reader.Read())
+      {
+        var nameId = reader.GetFieldValue<int>(0);
+        var eventId = reader.GetFieldValue<int>(1);
+        var date = reader.GetFieldValue<string>(2);
+
+        Console.WriteLine($"{nameId}|{eventId}|{date}");
+      }
     }
   }
 }
