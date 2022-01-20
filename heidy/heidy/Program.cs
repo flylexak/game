735a92ab34262584460586608d3c9f3716115cf9
flylexak <94125401+flylexak@users.noreply.github.com>
2022-01-20 20:00:47 +0400

    Update Program.cs

--- heidy/heidy/Program.cs
+++ heidy/heidy/Program.cs
@@ -11,15 +11,21 @@ namespace heidy
       connection.ConnectionString = "Host=10.46.1.131; Port=5432; Database=ITB15; Username=postgres; Password=12345";
       connection.Open();
       var command = connection.CreateCommand();
-      command.CommandText = @"SELECT * FROM ""qwzc.dates""";
+      command.CommandText = @"SELECT ""surname"", ""name"", ""otchestvo"", ""birthday"", ""number"", ""litera"" FROM ""F.studentforklasses"" D
+      LEFT JOIN ""F.students"" N ON(N.""ID"" = D.""studentsID"")
+      LEFT JOIN ""F.klases"" E ON(E.""ID"" = D.""klasesID"")";
+
       var reader = command.ExecuteReader();
       while (reader.Read())
       {
-        var nameId = reader.GetFieldValue<int>(0);
-        var eventId = reader.GetFieldValue<int>(1);
-        var date = reader.GetFieldValue<string>(2);
+        var surname = reader.GetFieldValue<string>(0);
+        var name = reader.GetFieldValue<string>(1);
+        var otchestvo = reader.GetFieldValue<string>(2);
+        var birthday = reader.GetFieldValue<string>(3);
+        var number = reader.GetFieldValue<int>(4);
+        var litera = reader.GetFieldValue<string>(5);
 
-        Console.WriteLine($"{nameId}|{eventId}|{date}");
+        Console.WriteLine($"{surname}|{name}|{otchestvo}|{birthday}|{number}|{litera}");
       }
     }
   }
